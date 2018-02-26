2268e53501c35c8ff04d5461df3f8615b00b6003
soflarb <soflarb@student.chalmers.se>
2018-02-26 20:56:56 +0100

    add comments to all lines in parallellsearch

--- src/main/amazed/solver/ForkJoinSolver.java
+++ src/main/amazed/solver/ForkJoinSolver.java
@@ -85,78 +85,101 @@ public class ForkJoinSolver
 
     private List<Integer> parallelSearch()
     {
-
         //initialize this solver
         if(visited.contains(current)){
             return null;
         }
+        //mark node as visited
+        visited.add(current);
+        
+        //create the player
         int player = maze.newPlayer(current);
-
+        
+        //push the current node to frontier
         frontier.push(current);
 
+        //enter the search loop
         while (!frontier.empty() && !finished.get()){
-
+            //take out the node next up for examination and call it "current"
             int current = frontier.pop();
-
+            
+            //check if current is a goal
             if (maze.hasGoal(current)) {
                 System.out.println("GOAL!");
+                //set the shared variable to inform everyone that the search is finished
                 finished.set(true);
-                // move player to goal
+                //move player to goal
                 maze.move(player, current);
-                // search finished: reconstruct and return path
+                //search finished: reconstruct and return path
                 return pathFromTo(start, current);
             }
-
+            
+            //current was not goal
+            //so now check if current is already visited. if so, skip the rest of the iteration
             if(visited.contains(current)){
                continue;
             }
             // mark node as visited
             visited.add(current);
-            // move player to current node
+            //move player to current
             maze.move(player, current);
-
+            //get the neighbors of current
             Set<Integer> neighbors = maze.neighbors(current);
 
-                // more than 2 equals two or more roads not visited by current fork. size is always at least 1 (the previous)
-                if(neighbors.size() > 2 && stepCounter >= forkAfter){
-                    System.out.println("GAFFEEEEEEL!");
-                    ArrayList<ForkJoinTask<List<Integer>>> forks = new ArrayList<>();
-
-                    for(int nb:neighbors){
-                        if(!visited.contains(nb)){
-                            predecessor.put(nb, current);
-                            ForkJoinSolver newSolver = new ForkJoinSolver(maze, nb,predecessor, forkAfter);
-                            forks.add(newSolver.fork());
-                        }
-                    }
-
-                    for (ForkJoinTask<List<Integer>> fork:forks) {
-                        List<Integer> path = fork.join();
-                        if(path != null){
-                            return path;
-                        }
-                    }
-                    if(!frontier.empty()) {
-                        continue;
-                    }
-                    return null;
-
-                }
-                else{
-                    System.out.println("Jag fick inga bestick");
-                    for(int nb: neighbors){
-                        if(!visited.contains(nb)){
-                            predecessor.put(nb, current);
-                            frontier.push(nb);
-                        }
+            //if there are more than one nodes to choose between for the next step, and there have been enough steps since last fork, do forking
+            // ("> 2" means two or more not visited by current fork. size is always at least 1 (the previous will always be there))
+            if(neighbors.size() > 2 && stepCounter >= forkAfter){
+                System.out.println("GAFFEEEEEEL!"); //TODO remove debug print
+                //make list to keep track of the forked tasks
+                ArrayList<ForkJoinTask<List<Integer>>> forks = new ArrayList<>();
+                //go through all the neighbors of current
+                for(int nb:neighbors){
+                    //if the neighbor is not visited
+                    if(!visited.contains(nb)){
+                        //add it to the path of predecessors
+                        predecessor.put(nb, current);
+                        //create a new solver
+                        ForkJoinSolver newSolver = new ForkJoinSolver(maze, nb, predecessor, forkAfter);
+                        //add it to the list of
+                        forks.add(newSolver.fork());
                     }
                 }
-                this.stepCounter ++;
+                //for each forked task
+                for (ForkJoinTask<List<Integer>> fork:forks) {
+                    //get the result from the fork
+                    List<Integer> path = fork.join();
+                    //if the path found was not null, return the path
+                    if(path != null){
+                        return path;
+                    }
+                }
+                //if there are still nodes to explore, keep searching
+                if(!frontier.empty()) {
+                    continue;
+                }
+                //if there was no path found, return null
+                return null;
+            }
+            //else if it was not time to fork
+            else {
+                System.out.println("Jag fick inga bestick");
+                //for each of the neighbors
+                for(int nb: neighbors){
+                    //if not visited
+                    if(!visited.contains(nb)){
+                        //put current as predecessor
+                        predecessor.put(nb, current);
+                        //push to frontier
+                        frontier.push(nb);
+                    }
+                }
+            }
+            //count the step
+            this.stepCounter ++;
         }
-        //dead end return the result of joining
+        //dead end, return the result of joining
         return null;
     }
-
 }
 
 /* QUESTIONS
